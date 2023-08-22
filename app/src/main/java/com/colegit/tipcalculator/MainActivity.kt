2047f1cf8f43d23aad479bd76bd8ba6c14313fa8
Cole <70454353+Colegit@users.noreply.github.com>
2023-08-21 17:15:35 -0700

    UI: Changed some ID names

--- app/src/main/java/com/colegit/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/colegit/tipcalculator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.colegit.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var baseAmount: EditText
+    private lateinit var tipSlider: SeekBar
+    private lateinit var tipAmount: TextView
+    private lateinit var totalToPay: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        baseAmount = findViewById(R.id.baseAmount)
+        tipSlider = findViewById(R.id.tipSlider)
+        tipAmount = findViewById(R.id.tipAmount)
+        totalToPay = findViewById(R.id.totalToPay)
     }
 }
